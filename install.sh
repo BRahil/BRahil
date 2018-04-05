f5b1117d2bdd0b1e24fdf0f90e9886a7ddf8cb44
BRahil <37744792+BRahil@users.noreply.github.com>
2018-04-05 18:08:37 +0000

    initdev

---
+++ install.sh
@@ -0,0 +1,15 @@
+#!/bin/bash
+mkdir bin
+mkdir licenses
+touch licenses
+mkdir makefile
+touch makefile
+mkdir sources
+mkdir gitignores
+touch main.sh
+cd gitignores 
+touch main.c
+touch main.cpp
+touch main.py
+touch main.tex
+
